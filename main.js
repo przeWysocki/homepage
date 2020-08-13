13307e4421ad481ac214b10f103150b8887a478f
Przemysław Wysocki <przewysocki@gmail.com>
2020-08-13 17:33:00 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+alert ('Czołem użytkowniku');
+
+console.log ('Witaj w konsoli, aktualnie nie mam nic do powiedzenia');
+
+const firstName='Przemek';
+const age='36';
+
+console.log (`Witaj w konsoli, nazywam się ${firstName} i mam ${age} lat`); 
