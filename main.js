638bebfa81261af9cd8238502c55a108b49fc959
Przemysław Wysocki <przewysocki@gmail.com>
2020-10-14 18:08:36 +0200

    flexbox & some other styles

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
-alertTTTTTTTTTT("Czołem użytkowniku");
 
-console.log("Witaj w konsoli, aktualnie nie mam nic do powiedzenia");
 
-const firstName = "Przemek";
-const age = "36";
+console.log(`Witaj w konsoli, aktualnie nie mam nic do powiedzenia`);
 
-console.log(`Witaj w konsoli, nazywam się ${firstName} i mam ${age} lat`);
+const names = 'Przemek';
+const ages = '36';
 
+console.log(`Witaj w konsoli, nazywam się ${names} i mam ${ages} lat`);
+
+
+
+const kupa = document.querySelector(".recipes__description--js");
+
+kupa.innerHTML = `dupa dupa dupa`;
+
+console.log(kupa.innerHTML);
+
+
+function greet (age, firstName) {
+ console.log (`Cześć nazywam się ${firstName} i mam ${age} lat`);
+}
+
+greet (36, `Przemek`);
+
+
+const secondGreet = (fingers, secondName) => {
+    console.log (`Czołem, nazywam się ${secondName} i mam ${fingers} palców`)
+}
+
+secondGreet (20, `Jerzy`);
+
+
+const greetings = { 
+    infos: (strength, thirdName) => {
+        console.log(`Witaj, mam na imię ${thirdName} i mam ${strength} siły`) 
+    }
+}
+greetings.infos (`50%`, `Tytus`);
